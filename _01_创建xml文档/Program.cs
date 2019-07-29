6f1d8bbd998179af0c1941018006a23be765c025
ocxz <2252779530@qq.com>
2019-07-29 09:23:15 +0800

    添加项目文件。

---
+++ _01_创建xml文档/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_创建xml文档
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
